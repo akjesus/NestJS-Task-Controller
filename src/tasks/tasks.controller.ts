4bd55848f2867234f9784ad8fdce94f3e327892b
Otong Akan <cutytongy@gmail.com>
2023-01-05 14:30:26 +0100

    added routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
-    @Get()
-   getAllTasks() {
-    return this.tasksService.getAllTasks()
-   }
-
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+  @Get('/:id')
+  getTaskById(@Param('id') id:string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+  @Delete('/:id')
+  deleteTask(@Param('id') id:string): void {
+    return this.tasksService.deleteTask(id)
+  }
+  @Patch('/:id/status')
+  updateTask(@Param('id') id:string, @Body('status') status: TaskStatus): Task {
+    return this.tasksService.updateTaskStatus(id, status)
+  }
 }
